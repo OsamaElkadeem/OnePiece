2b9baa1ac1bf2c812680d189bc5905d8aa400adb
AhmedAbdelbasetAli <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2022-05-17 02:14:29 +0200

    Setup database and connect to backend

--- OnePiece/src/main/java/module-info.java
+++ OnePiece/src/main/java/module-info.java
@@ -1,16 +1,13 @@
 module com.example.onepiece {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
-    requires java.sql;
-    requires com.h2database;
-    requires javax.persistence;
 
     opens com.example.onepiece to javafx.fxml;
     exports com.example.onepiece;
-    exports com.example.onepiece.Controller;
-    opens com.example.onepiece.Controller to javafx.fxml;
+    
 }
